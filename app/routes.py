18bf48b66727fb0bafcca909cce7e685692a57e3
Utsav Bajpai <utsavbajpai22@yahoo.com>
2018-01-29 19:47:58 -0500

    First Commit

---
+++ app/routes.py
@@ -0,0 +1,44 @@
+from app import app
+from geopy import geocoders
+'''gn = geocoders.GeoNames()'''
+import requests
+from flask import request
+import json
+from flask import render_template
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/placedetails')
+def placedetails():
+    zipcode = request.args.get('zipcode')
+    print (zipcode)
+    send_url = 'http://freegeoip.net/json'
+    google_geocodeurl = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + zipcode
+    r = requests.get(send_url)
+    j = json.loads(r.text)
+    lat = j['latitude']
+    lon = j['longitude']
+
+    zipcoderequest = requests.get(google_geocodeurl)
+    zipcodejson = json.loads(zipcoderequest.text)
+
+    '''zipcodelatitude = zipcodejson[results[0].geometry.location.lat()]'''
+    ''' zipcodelongitude = zipcodejson[results[0].geometry.location.lng()] '''
+
+    zipcodelatitude= zipcodejson['results'][0]['geometry']['location']['lat']
+    zipcodelongitude = zipcodejson['results'][0]['geometry']['location']['lng']
+
+    airquality_url = 'https://api.breezometer.com/baqi/?lat=' + str(zipcodelatitude) + '&lon=' + str(zipcodelongitude) +'&key=74a3fdfe934f4e5c8b0cc50edfb225bd'
+
+    airquality_request = requests.get(airquality_url)
+    airquality_json = json.loads(airquality_request.text)
+
+    ''' print (airquality_json) '''
+
+    k = (airquality_json['breezometer_description'])
+    aqi = (airquality_json['breezometer_aqi'])
+    rr = (airquality_json['random_recommendations']['children'])
+    return render_template('placedetails.html', airqualityindex=aqi, co=rr)
