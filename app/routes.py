fe6028596342d18b7df0c24e3c5158fdc5ce9be6
Sharoon Srivastava <srivas15@purdue.edu>
2018-01-30 00:23:07 -0800

    adding frontend

--- app/routes.py
+++ app/routes.py
@@ -9,24 +9,34 @@ from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
-
-@app.route('/placedetails')
-def placedetails():
-    zipcode = request.args.get('zipcode')
-    print (zipcode)
     send_url = 'http://freegeoip.net/json'
-    google_geocodeurl = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + zipcode
     r = requests.get(send_url)
     j = json.loads(r.text)
     lat = j['latitude']
     lon = j['longitude']
 
+    airquality_url = 'https://api.breezometer.com/baqi/?lat=' + str(lat) + '&lon=' + str(lon) +'&key=74a3fdfe934f4e5c8b0cc50edfb225bd'
+    airquality_request = requests.get(airquality_url)
+    airquality_json = json.loads(airquality_request.text)
+
+    aqi = (airquality_json['breezometer_aqi'])
+    rr = (airquality_json['random_recommendations']['outside'])
+
+    return render_template('index.html', airqualityindex=aqi, rr=rr)
+
+@app.route('/placedetails')
+def placedetails():
+    zipcode = request.args.get('zipcode')
+    print (zipcode)
+    # send_url = 'http://freegeoip.net/json'
+    google_geocodeurl = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + zipcode
+    # r = requests.get(send_url)
+    # j = json.loads(r.text)
+    # lat = j['latitude']
+    # lon = j['longitude']
+
     zipcoderequest = requests.get(google_geocodeurl)
     zipcodejson = json.loads(zipcoderequest.text)
-
-    '''zipcodelatitude = zipcodejson[results[0].geometry.location.lat()]'''
-    ''' zipcodelongitude = zipcodejson[results[0].geometry.location.lng()] '''
 
     zipcodelatitude= zipcodejson['results'][0]['geometry']['location']['lat']
     zipcodelongitude = zipcodejson['results'][0]['geometry']['location']['lng']
@@ -40,5 +50,5 @@ def placedetails():
 
     k = (airquality_json['breezometer_description'])
     aqi = (airquality_json['breezometer_aqi'])
-    rr = (airquality_json['random_recommendations']['children'])
-    return render_template('placedetails.html', airqualityindex=aqi, co=rr)
+    rr = (airquality_json['random_recommendations']['outside'])
+    return render_template('index.html', airqualityindex=aqi, rr=rr)
